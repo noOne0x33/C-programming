de92108ac540a8753583eee3438491231cd8e10b
Tanmoy Barman <143665940+noOne0x33@users.noreply.github.com>
2023-09-23 00:44:47 +0530

    problem_6

---
+++ problem_6.c
@@ -0,0 +1,11 @@
+/***
+*write a program to print te triangle pattern
+***/
+#include <stdio.h>
+
+int main() {
+    printf("    *    \n");
+    printf("   * *   \n");
+    printf("  * * *  \n");
+    return 0;
+}
